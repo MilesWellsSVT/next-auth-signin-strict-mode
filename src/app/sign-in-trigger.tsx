34a93ccd435f657fa89d26e9ff1e079de96b2e6a
Miles Wells <miles@svtrobotics.com>
2023-09-25 17:04:42 -0400

    simplify

--- src/app/sign-in-trigger.tsx
+++ src/app/sign-in-trigger.tsx
@@ -1,21 +1,19 @@
 'use client';
 
 import { signIn, useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { duendeProvider } from './auth';
 
 type SignInTriggerProps = React.PropsWithChildren;
 
 export function SignInTrigger({ children }: SignInTriggerProps) {
 	const { status } = useSession();
-  const [authInitiated, setAuthInitiated] = useState(false);
 
 	useEffect(() => {
-		if (!authInitiated && status === 'unauthenticated') {
-			setAuthInitiated(true);
+		if (status === 'unauthenticated') {
 			void signIn(duendeProvider.id);
 		}
-	}, [authInitiated, status]);
+	}, [status]);
 
 	if (status === 'unauthenticated') return null;
 
